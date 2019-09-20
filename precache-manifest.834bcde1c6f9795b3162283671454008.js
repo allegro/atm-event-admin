1f414df199c6332d0f97a8bf79fb140b1188c227
Bartosz Galek <bartosz.galek@allegro.pl>
2019-09-20 12:54:17 +0200

    Updates

---
+++ precache-manifest.834bcde1c6f9795b3162283671454008.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5901d0082351c9acb2c947af24649149",
+    "url": "/atm-event-admin/index.html"
+  },
+  {
+    "revision": "9d3243cc56b8de55ae9b",
+    "url": "/atm-event-admin/static/js/2.29f3f715.chunk.js"
+  },
+  {
+    "revision": "995b9fdd9d93370915ff",
+    "url": "/atm-event-admin/static/js/main.7f8750e3.chunk.js"
+  },
+  {
+    "revision": "0b3505d48f4bab9afdb4",
+    "url": "/atm-event-admin/static/js/runtime~main.935e9eab.js"
+  }
+]);
